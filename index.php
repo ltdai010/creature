4fb01f9dbaa0d86492b5c326047a7169845a05d3
ltdai010 <47277582+ltdai010@users.noreply.github.com>
2020-04-09 13:27:56 +0700

    up

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Creature</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+	<!-- <div id="fb-root"></div>
+	<script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v6.0&appId=226267398624862&autoLogAppEvents=1"></script>
+	<form action="search.php" method="get">
+		<input type="text" name = "key" placeholder="Search.."/>
+		<input type="submit" value="Search"/>
+	</form>
+	<img src="bosat.jpg" class="chooseButton" id="boSat">
+	<img src="mammals.jpg" class="chooseButton" id="mammal">
+	<img src="insects.jpg" class="chooseButton" id="insect">
+	<img src="fish.jpg" class="chooseButton" id="fish">
+	<img src="bird.jpg" class="chooseButton" id="bird"> -->
+	<div class="fb-comments" data-href="https://developers.facebook.com/docs/plugins/comments#configurator" data-width="" data-numposts="5"></div>
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+		if (isset($_GET['key']) && $_GET['key'] != ''){
+						
+			// save the keywords from the url
+			$key = trim($_GET['key']);
+
+			// create a base query and words string
+			$query_string = "SELECT * FROM products WHERE ";
+			$display_words = "";
+
+			// seperate each of the productLine
+			$productLine = explode(' ', $key); 
+			foreach($productLine as $word){
+				$query_string .= " productLine LIKE '%".$word."%' OR ";
+				$display_words .= $word." ";
+			}
+			$query_string = substr($query_string, 0, strlen($query_string) - 3);
+
+			// connect to the database
+			$conn = mysqli_connect('localhost', 'root', '', 'classicmodels');
+
+			$query = mysqli_query($conn, $query_string);
+			$result_count = mysqli_num_rows($query);
+
+			// check to see if any results were returned
+			if ($result_count > 0){
+							
+				// display search result count to user
+				echo '<br /><div class="right"><b><u>'.$result_count.'</u></b> results found</div>';
+				echo 'Your search for <i>'.$display_words.'</i> <hr /><br />';
+
+				echo '<table class="search">';
+
+				// display all the search results to the user
+				while ($row = mysqli_fetch_assoc($query)){
+								
+					echo "id: " . $row["productCode"]. " - Name: " . $row["productName"]. " "
+            . $row["productLine"]. "<br>";
+				}
+
+				echo '</table>';
+			}
+			else
+			echo 'No results found. Please search something else.';
+		}
+		else
+		echo '';
+	?>
+</body>
+</html>
