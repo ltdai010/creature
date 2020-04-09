e337a9d3c863ada1545efb5468ca1516ac82e18c
ltdai010 <47277582+ltdai010@users.noreply.github.com>
2020-04-09 13:31:00 +0700

    up

--- index.php
+++ index.php
@@ -6,8 +6,6 @@
 	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
-	<!-- <div id="fb-root"></div>
-	<script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v6.0&appId=226267398624862&autoLogAppEvents=1"></script>
 	<form action="search.php" method="get">
 		<input type="text" name = "key" placeholder="Search.."/>
 		<input type="submit" value="Search"/>
@@ -17,6 +15,5 @@
 	<img src="insects.jpg" class="chooseButton" id="insect">
 	<img src="fish.jpg" class="chooseButton" id="fish">
 	<img src="bird.jpg" class="chooseButton" id="bird"> -->
-	<div class="fb-comments" data-href="https://developers.facebook.com/docs/plugins/comments#configurator" data-width="" data-numposts="5"></div>
 </body>
 </html>
