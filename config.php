53c21725c091abe14e1c20b7174fde0c951fc641
ltdai010 <47277582+ltdai010@users.noreply.github.com>
2020-04-11 23:15:21 +0700

    up'

--- config.php
+++ config.php
@@ -1,13 +1,6 @@
 <?php 
 define('DB_SERVER', 'us-cdbr-iron-east-01.cleardb.net');
-define('DB_USERNAME', 'b4ef8d97f5a3ce');
-define('DB_PASSWORD', 'eed524752aa4497 ');
+define('DB_USER', 'b4ef8d97f5a3ce');
+define('DB_PASS', 'b561fd24760cb1d');
 define('DB_NAME','heroku_1cb1d5ce05fc534');
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
  ?>
