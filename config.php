01db4700281e2e63f4c17c6af7892b8b42618589
ltdai010 <47277582+ltdai010@users.noreply.github.com>
2020-04-12 20:46:40 +0700

    up

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('DB_USER', 'b4ef8d97f5a3ce');
 define('DB_PASS', 'b561fd24760cb1d');
 define('DB_NAME','heroku_1cb1d5ce05fc534');
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
  mysqli_set_charset($link, 'UTF8');
 // Check connection
 if($link === false){
